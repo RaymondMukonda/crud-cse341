ba17df2f277da368a251ee7b9b7a65be1b1b4f3c
Raymond Mukonda <Raymondkogen@gmail.com>
2026-07-17 22:01:02 +0200

    finished week03 part1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import itemRoutes from './routes/itemRoutes.js';   
 import { swaggerUi, swaggerSpec } from './swagger.js';
 
 dotenv.config();
@@ -13,15 +14,15 @@ app.use(express.json());
 
 connectDB();
 
-
 // Swagger docs route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Routes
 app.use('/api/users', userRoutes);
+app.use('/api/items', itemRoutes);   
 
 app.get('/', (req, res) => {
-  res.send('API is running...');
+    res.send('API is running...');
 });
 
 const PORT = process.env.PORT || 3000;
